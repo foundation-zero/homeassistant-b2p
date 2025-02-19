bca097e299fc43fe2508ce840f49dc1b8605f652
Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
2025-02-19 13:57:45 +0100

    Improve B2P integration to prevent PDC overloading

--- const.py
+++ const.py
@@ -4,4 +4,6 @@ DOMAIN = "b2p"
 CONF_B2P_HOST = "host"
 CONF_B2P_PDC = "pdc"
 CONF_B2P_CHANNEL = "channel"
-DATA_SOCKET = "b2p_socket"
+DATA_PDC = "b2p_pdc"
+B2P_PDC_THROTTLE = 0.1
+B2P_FADE_TIME = 0.09
--- light.py
+++ light.py
@@ -1,17 +1,29 @@
-from homeassistant.const import CONF_NAME
-from homeassistant.core import HomeAssistant
+import asyncio
+import socket
+from time import monotonic
+
+import voluptuous as vol
+
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
+    PLATFORM_SCHEMA_BASE,
     ColorMode,
     LightEntity,
-    LightEntityDescription,
-    PLATFORM_SCHEMA_BASE,
 )
+from homeassistant.const import CONF_NAME
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-import socket
-import voluptuous as vol
-from .const import DOMAIN, CONF_B2P_HOST, CONF_B2P_PDC, CONF_B2P_CHANNEL, DATA_SOCKET
+
+from .const import (
+    B2P_FADE_TIME,
+    B2P_PDC_THROTTLE,
+    CONF_B2P_CHANNEL,
+    CONF_B2P_HOST,
+    CONF_B2P_PDC,
+    DATA_PDC,
+    DOMAIN,
+)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA_BASE.extend(
     {
@@ -23,65 +35,105 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA_BASE.extend(
 )
 
 
+def _pdc_id(host: str, pdc: int) -> str:
+    return f"{host}_{pdc}"
+
+
 def setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
     add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
-    sock = hass.data.get(DATA_SOCKET, socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
+    pdcs: dict[str, B2pPdc] = hass.data.get(DATA_PDC, {})
+    pdc_id = _pdc_id(config[CONF_B2P_HOST], config[CONF_B2P_PDC])
+    pdc = pdcs.get(pdc_id) or B2pPdc(config[CONF_B2P_HOST], config[CONF_B2P_PDC])
+    pdcs[pdc_id] = pdc
+    hass.data[DATA_PDC] = pdcs
+
     add_entities(
         [
-            RenaLight(
+            B2pLight(
                 config[CONF_NAME],
-                config[CONF_B2P_HOST],
-                config[CONF_B2P_PDC],
                 config[CONF_B2P_CHANNEL],
-                sock,
+                pdc,
             )
         ]
     )
 
 
-class RenaLight(LightEntity):
+class B2pPdc:
+    """Representation of a B2P PDC.
+
+    Keeps track of the channels to prevent overloading PDC with too many commands.
+    """
+
+    def __init__(self, host: str, pdc: int) -> None:
+        self._host = host
+        self._pdc = pdc
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._brightnesses: dict[int, int] = {}
+        self._last_message = 0
+        self._is_scheduled = False
+
+    async def set_brightness(self, channel: int, brightness: int) -> None:
+        """Set the brightness of a channel."""
+        time_since_last_message = monotonic() - self._last_message
+        self._brightnesses[channel] = brightness
+        if (
+            not self._is_scheduled
+        ):  # if already scheduled, rely on that update to send brightness
+            # negative sleeps execute immediately
+            self._is_scheduled = True
+            await asyncio.sleep(B2P_PDC_THROTTLE - time_since_last_message)
+            channels = [
+                f"{channel}={int(brightness / 2.55)}"
+                for channel, brightness in self._brightnesses.items()
+            ]
+            self._sock.sendto(
+                bytes(
+                    f"@FADE( {self._pdc}, {int(B2P_FADE_TIME * 1000)} ) SOLL {{ {", ".join(channels)} }}\n",
+                    "ascii",
+                ),
+                (self._host, 50000),
+            )
+            self._last_message = monotonic()
+            self._is_scheduled = False
+
+    def brightness(self, channel: int) -> int:
+        return self._brightnesses.get(channel, 0)
+
+    @property
+    def id(self):
+        return _pdc_id(self._host, self._pdc)
+
+
+class B2pLight(LightEntity):
+    """Representation of a B2P light."""
+
     _attr_color_mode = ColorMode.BRIGHTNESS
     _attr_supported_color_modes = {ColorMode.BRIGHTNESS}
     _attr_has_entity_name = True
-    _attr_should_poll = False
+    _attr_should_poll = True
 
-    def __init__(self, name: str, host: str, pdc: int, channel: int, sock):
+    def __init__(self, name: str, channel: int, pdc: B2pPdc):
         self._is_on = False
-        self._brightness = 0
         self._attr_name = name
-        self._attr_unique_id = f"{host}_{pdc}_{channel}"
-        self._host = host
+        self._attr_unique_id = f"{pdc.id}_{channel}"
         self._pdc = pdc
         self._channel = channel
-        self._sock = sock
 
     @property
     def is_on(self):
         """If the switch is currently on or off."""
-        return self._is_on
+        return self.brightness > 0
 
     @property
     def brightness(self):
-        return self._brightness
+        return self._pdc.brightness(self._channel)
 
-    def turn_on(self, **kwargs):
-        """Turn the switch on."""
-        self._is_on = True
-        fade_time_ms = 100
-        self._brightness = kwargs.get(ATTR_BRIGHTNESS, 255)
+    async def async_turn_on(self, **kwargs):
+        await self._pdc.set_brightness(self._channel, kwargs.get(ATTR_BRIGHTNESS, 255))
 
-        self._sock.sendto(
-            bytes(
-                f"@FADE( {self._pdc}, {fade_time_ms} ) SOLL {{ {self._channel}={int(self._brightness / 2.55)} }}\n",
-                "ascii",
-            ),
-            (self._host, 50000),
-        )
-
-    def turn_off(self, **kwargs):
-        """Turn the switch off."""
-        self._is_on = False
+    async def async_turn_off(self, **kwargs):
+        await self._pdc.set_brightness(self._channel, 0)
